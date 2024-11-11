4dfb044f1a98d5fad41bbf3601242a9c9248f3a2
Manal <manal.lounadi@gmail.com>
2024-11-11 12:22:40 +0100

    d c

--- api/fastapi_app.py
+++ api/fastapi_app.py
@@ -17,37 +17,32 @@ import pandas as pd
 import joblib
 import os
 
-
-#charger le modèle et le scaler
+# Charger le modèle LightGBM
 current_directory = os.path.dirname(os.path.realpath(__file__)) 
 model = joblib.load(os.path.join(current_directory, "lightgbm_model_f.joblib"))
-#scaler = joblib.load(os.path.join(current_directory,  "scaler_lgbm.joblib"))
 
-
-#création de l'application FastAPI
+# Création de l'application FastAPI
 app = FastAPI()
 
-#la structure de la requête de prédiction
+# La structure de la requête de prédiction
 class ClientData(BaseModel):
     SK_ID_CURR: int
 
-#chargement des données des nouveaux clients (CSV)
+# Chargement des données des nouveaux clients (CSV)
 new_clients_df = pd.read_csv(os.path.join(current_directory, 'df_nouveaux_clients.csv'))
-
 
 # Route de redirection de '/' vers '/clients'
 @app.get("/")
 def read_root():
     return RedirectResponse(url="/clients")
 
-#la route (url: http://127.0.0.1:8000/clients) pour la liste des id clients: SK_ID_CURR
+# La route (url: http://127.0.0.1:8000/clients) pour la liste des id clients: SK_ID_CURR
 @app.get("/clients")
 def get_clients():
     """Retourner la liste des SK_ID_CURR"""
     return new_clients_df['SK_ID_CURR'].tolist()
 
-#la route (http://127.0.0.1:8000/predict) pour faire une prédiction pour un client spécifique
-#je récupère ces prédictions via Streamplit
+# La route (http://127.0.0.1:8000/predict) pour faire une prédiction pour un client spécifique
 @app.post("/predict")
 def predict(client_data: ClientData):
     """Faire une prédiction pour un client spécifique"""
@@ -62,14 +57,12 @@ def predict(client_data: ClientData):
         if client_row.empty:
             return {"error": "Client not found"}
 
-        # Préparer les données pour le modèle (supprimer SK_ID_CURR et appliquer le scaler)
+        # Préparer les données pour le modèle (supprimer SK_ID_CURR)
         client_features = client_row.drop(columns=["SK_ID_CURR"]).values
-        #client_scaled = scaler.transform(client_features)
         
         # Effectuer la prédiction
-        #prediction_proba = float(model.predict_proba(client_scaled)[:, 1][0])  # Convertir en float natif
         prediction_proba = float(model.predict_proba(client_features)[:, 1][0])  # Convertir en float natif
-        prediction_label = int(prediction_proba > 0.45)  # Convertir en int natif
+        prediction_label = int(prediction_proba > 0.53)  # Convertir en int natif
         
         return {
             "SK_ID_CURR": int(client_id),  # Convertir en int natif
